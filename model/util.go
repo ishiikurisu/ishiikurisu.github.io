40a02c22dc6ee465a03059e839288f193c2697c9
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-12-06 21:19:48 -0200

    Trying to send an email with my site

--- model/util.go
+++ model/util.go
@@ -1,7 +1,9 @@
 package model
 
 import "os"
-import "net/smtp"
+import "os/exec"
+import "fmt"
+import "errors"
 
 // Gets the port for releasing the server
 func GetPort() string {
@@ -16,21 +18,24 @@ func GetPort() string {
 
 // Sends a simple email
 func SendSimpleMail(recipient, message string) error {
-    mail := []byte("From: " + os.Getenv("USERNAME") + "\r\n" +
-                   "To: " + recipient + "\r\n" +
-                   "Subject: Automatic Mail\r\n\r\n" +
-                   message + "\r\n")
-    auth := smtp.PlainAuth("",
-                           os.Getenv("USERNAME"),
-                           os.Getenv("PASSWORD"),
-                           "smtp.gmail.com")
-    oops := smtp.SendMail("smtp.gmail.com:587",
-                          auth,
-                          os.Getenv("USERNAME"),
-                          []string{
-                              recipient,
-                          },
-                          mail)
+    mailgunApi := fmt.Sprintf("'%s'", os.Getenv("MAILGUN_API"))
+    mailgunHttp := os.Getenv("MAILGUN_HTTP")
+    mailgunUser := fmt.Sprintf("from='Mailgun Sandbox <%s>'", os.Getenv("MAILGUN_USER"))
+    to := fmt.Sprintf("to='Liberdade Organização <%s>'", recipient)
+    text := fmt.Sprintf("text='%s'", message)
+
+    cmd := exec.Command("curl",
+                        "-s",
+                        "--user", mailgunApi,
+                        mailgunHttp,
+                        "-F", mailgunUser,
+                        "-F", to,
+                        "-F", "subject='Automatic Email'",
+                        "-F", text)
+    output, oops := cmd.Output()
+    if string(output) == "Forbidden" {
+        oops = errors.New(string(output))
+    }
 
     return oops
 }
