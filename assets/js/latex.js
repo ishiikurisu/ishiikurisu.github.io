7fe662c80fc9932449c7b52a5d4d5993d84cd17a
Cristiano Alves da Silva Junior <cristiano.junior@mbra.com.br>
2018-09-26 14:11:20 -0300

    Enabling preview of ASCIIMath as well

--- assets/js/latex.js
+++ assets/js/latex.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
     $('#latex-input').keyup(function() {
         var input = $('#latex-input').val();
+        var sep = ($("input[name=options-radions]:checked").val())? "$$" : "`";
         var output = 'Digite a equação aqui...';
         
         if (input.length > 0) {
-            output = "$$" + input + "$$";
+            output = sep + input + sep;
         }
             
         $('#latex-output').html(output);
-        MathJax.Hub.Typeset()
+        MathJax.Hub.Typeset();
     });
 });
