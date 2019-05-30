6e067912a27a0a57521ca6f977219ecea7e09a71
Cris Silva Jr <ishiikurisu@yahoo.com>
2019-05-30 19:12:32 -0300

    Using Liberdade's blog framework to handle notes

--- assets/js/notes/index.js
+++ assets/js/notes/index.js
@@ -1,50 +1,30 @@
-function get(url, callback) {
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.onload = function() {
-        if (request.status >= 200 && request.status < 400) {
-            var data = JSON.parse(request.responseText);
-            callback(data);
-        } else {
-            callback({error: "Bad request"});
-        }
-    };
-    request.onerror = function() {
-        callback({error: "Connection error"});
-    };
-    request.send();
-}
-
-function draw(content, posts) {
-    var limit = posts.length;
-    var outlet = "";
-
-    for (var i = 0; i < limit; i++) {
-        var post = posts[i];
-        outlet += `
-        <div class="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-2">
-            <h3 class="content-subhead">
-                <a class="joe-ribbon-header"
-                   href="/notes/note.html?which=`+post.path+`&title=`+encodeURI(post.title)+`">
-                    `+post.title+`
-                </a>
-            </h3>
-            <p>
-                `+post.description+`
-            </p>
-        </div>
-        `;
-    }
-
-    content.innerHTML = outlet;
-}
-
-get("https://www.gitcdn.xyz/repo/ishiikurisu/blog-posts/master/index.json", function(data) {
+var blog = new GithubBlog('ishiikurisu/blog-posts');
+blog.loadIndex(function(posts) {
     var content = document.getElementById('content');
 
-    if (data.error) {
+    if (posts.error) {
         content.innerHTML = "<p>Erro inesperadp :(</p><p>Tente novamente mais tarde</p>";
     } else {
-        draw(content, data);
+        var limit = posts.length;
+        var outlet = "";
+
+        for (var i = 0; i < limit; i++) {
+            var post = posts[i];
+            outlet += `
+            <div class="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-2">
+                <h3 class="content-subhead">
+                    <a class="joe-ribbon-header"
+                       href="/notes/note.html?which=${post.path}&title=${encodeURI(post.title)}">
+                        ${post.title}
+                    </a>
+                </h3>
+                <p>
+                    ${post.description}
+                </p>
+            </div>
+            `;
+        }
+
+        content.innerHTML = outlet;
     }
 });
--- assets/js/notes/note.js
+++ assets/js/notes/note.js
@@ -1,45 +1,30 @@
-function get(url, callback) {
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.onload = function() {
-        if (request.status >= 200 && request.status < 400) {
-            callback(request.responseText);
-        } else {
-            callback({error: "Bad request"});
-        }
-    };
-    request.onerror = function() {
-        callback({error: "Connection error"});
-    };
-    request.send();
-}
-
-function draw(content, post, convert) {
-    var outlet = `
-        <h2 class="content-head is-center">`+ post.title +`</h2>
-    `;
-    var body = post.body;
-    if (convert) {
-        var md = new Remarkable();
-        body = md.render(body);
-    }
-    outlet += body;
-    content.innerHTML = outlet;
-    MathJax.Hub.Typeset();
-}
-
-// MAIN FUNCTION
-
 var url = new URL(window.location.href);
 var which = url.searchParams.get("which");
 var title = decodeURI(url.searchParams.get("title"));
+var blog = new GithubBlog('ishiikurisu/blog-posts');
 
-get("https://www.gitcdn.xyz/repo/ishiikurisu/blog-posts/master/" + which, function(data) {
+blog.loadPost(`/${which}`, function(data) {
     var content = document.getElementById('content');
 
     if (data.error) {
-        content.innerHTML = "<p>Erro inesperadp :(</p><p>Tente novamente mais tarde</p>";
+        console.log(data.error);
+        content.innerHTML = "<p>Erro inesperado :(</p><p>Tente novamente mais tarde</p>";
     } else {
-        draw(content, {title: title, body: data}, which.substr(-2) === "md");
+        var post = {
+            title: title,
+            body: data
+        };
+        var convert = which.substr(-2) === "md";
+        var outlet = `
+            <h2 class="content-head is-center">`+ post.title +`</h2>
+        `;
+        var body = post.body;
+        if (convert) {
+            var md = new Remarkable();
+            body = md.render(body);
+        }
+        outlet += body;
+        content.innerHTML = outlet;
+        MathJax.Hub.Typeset();
     }
 });
