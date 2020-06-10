ccaafea7ec0e0629de0f74dacd48f0bd874f97a7
Cris Silva Jr <ishiikurisu@yahoo.com>
2020-06-09 22:57:41 -0300

    Render tables on notes

--- assets/js/notes/note.js
+++ assets/js/notes/note.js
@@ -2,6 +2,49 @@ var url = new URL(window.location.href);
 var which = url.searchParams.get("which");
 var blog = new GithubBlog('ishiikurisu/notes');
 
+function renderCsv(csv) {
+    var html = `<table class="pure-table pure-table-bordered">`;
+    var rows = csv.split("\n");
+    var rowCount = rows.length;
+    var firstLine = true;
+    var i = 0;
+
+    for (var j = 0; j < rowCount; j++) {
+        var line = "";
+        var row = rows[j];
+        var fields = row.split(",");
+        var fieldCount = fields.length;
+
+        if (row.length === 0) {
+            continue;
+        }
+
+        if (firstLine) {
+            line += "<br/><thead><tr>";
+            for (i = 0; i < fieldCount; i++) {
+                line += `<th>${fields[i]}</th>`;
+            }
+            line += "</tr></thead><tbody>";
+            firstLine = false;
+        } else {
+            line += "<tr>";
+            for (i = 0; i < fieldCount; i++) {
+                line += `<td>${fields[i]}</td>`;
+            }
+            line += "</tr>";
+        }
+
+        html += line;
+    }
+
+    if (rowCount > 0) {
+        html += "</tbody>";
+    }
+    html += "</table>";
+
+    return html
+}
+
 blog.loadPost(`/${which}`, function(data) {
     var content = document.getElementById('content');
 
@@ -10,9 +53,15 @@ blog.loadPost(`/${which}`, function(data) {
         content.innerHTML = "<p>Erro inesperado :(</p><p>Tente novamente mais tarde</p>";
     } else {
         var body = data;
-        if (which.substr(-2) === "md") {
-            var md = new Remarkable();
-            body = md.render(body);
+        var parts = which.split('.');
+        switch (parts[parts.length - 1]) {
+            case "md":
+                var md = new Remarkable();
+                body = md.render(body);
+                break;
+            case "csv":
+                body = renderCsv(body);
+                break;
         }
         content.innerHTML = body;
         MathJax.Hub.Typeset();
