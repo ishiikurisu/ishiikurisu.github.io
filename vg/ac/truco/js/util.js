0543de3f5a5dde6641e0c9fe34a2a9b9fc50bf0a
Cris Silva Jr <ishiikurisu@yahoo.com>
2020-07-07 22:29:39 -0300

    動物の森

---
+++ vg/ac/truco/js/util.js
@@ -0,0 +1,31 @@
+function shuffle(array) {
+    let counter = array.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
+
+function coinFlip(heads, tails) {
+    return [heads, tails][Math.floor(Math.random() * 2)];
+}
+
+function ready(fn) {
+    if (document.readyState != 'loading'){
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
